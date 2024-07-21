8f550514fdeef0585960cc50826389b3e4a9d7ec
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-21 19:26:01 +0300

    Interactive Auto Rendering Example

---
+++ Interactive Auto Rendering/Interactive Auto Rendering/Interactive Auto Rendering/Program.cs
@@ -0,0 +1,35 @@
+using Interactive_Auto_Rendering.Client.Pages;
+using Interactive_Auto_Rendering.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Interactive_Auto_Rendering.Client._Imports).Assembly);
+
+app.Run();
