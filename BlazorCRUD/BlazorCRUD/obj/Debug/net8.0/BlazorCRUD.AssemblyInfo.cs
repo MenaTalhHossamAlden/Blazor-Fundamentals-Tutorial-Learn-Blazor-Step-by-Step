fd92a0372f3ff6371fdbe72096b0edece672de4d
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-22 08:35:26 +0300

    creating a database and connecting it to the project

--- BlazorCRUD/BlazorCRUD/obj/Debug/net8.0/BlazorCRUD.AssemblyInfo.cs
+++ BlazorCRUD/BlazorCRUD/obj/Debug/net8.0/BlazorCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fb290d9a3b65d1d617606c192676d620391887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b71d32fd41d752ea51268a789bc23a12a06283d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
