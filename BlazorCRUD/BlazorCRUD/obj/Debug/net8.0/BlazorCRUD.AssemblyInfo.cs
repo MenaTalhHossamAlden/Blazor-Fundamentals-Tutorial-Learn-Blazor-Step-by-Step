2b71d32fd41d752ea51268a789bc23a12a06283d
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-07-22 07:09:49 +0300

    scaffolding the CRUD pages

--- BlazorCRUD/BlazorCRUD/obj/Debug/net8.0/BlazorCRUD.AssemblyInfo.cs
+++ BlazorCRUD/BlazorCRUD/obj/Debug/net8.0/BlazorCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea909b6c78858627e141d1525b4128a116d86f0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fb290d9a3b65d1d617606c192676d620391887")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
